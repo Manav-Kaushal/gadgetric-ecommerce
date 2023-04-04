60c7094be9cd7d4b1425d531fcb3529a64378a28
Manav Kaushal <manavkaushal756@gmail.com>
2023-04-04 12:54:35 +0530

    camera and shadows done

---
+++ src/canvas/Backdrop.tsx
@@ -0,0 +1,37 @@
+import React, { useRef } from "react";
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
+
+type Props = {};
+
+const Backdrop = (props: Props) => {
+  const shadows = useRef();
+
+  return (
+    <AccumulativeShadows
+      ref={shadows}
+      frames={60}
+      position={[0, 0, -0.14]}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      alphaTest={0.85} // set transparency of shadows
+      temporal // smooth out the edges of shadows over time
+    >
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[-5, 5, -9]}
+      />
+    </AccumulativeShadows>
+  );
+};
+
+export default Backdrop;
